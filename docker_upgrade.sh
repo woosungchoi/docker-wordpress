ae61d6ed1a175d97c51d7fd1109a675767e1cdd0
WoosungChoi <solut2000@gmail.com>
2020-09-11 14:51:30 +0900

    Update docker_upgrade.sh

--- docker_upgrade.sh
+++ docker_upgrade.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /your/path/to/docker-wordpress/
+cd /your/path/to/wordpress/
 
 $COMPOSE pull && $DOCKER pull woosungchoi/fpm-alpine && $COMPOSE up --build -d && $DOCKER image prune -f
